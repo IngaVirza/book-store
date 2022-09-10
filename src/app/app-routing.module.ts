6a4db2590918602169224db680f964c57e4b7c43
tokin89 <tokin8989@gmail.com>
2022-09-10 21:33:11 +0300

    init: init app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageMainComponent } from './pages/page-main/page-main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: PageMainComponent },
+	{ path: '**', redirectTo: '/', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageMainComponent } from './pages/page-main/page-main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageMainComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
