3fff1889db2e4e598d6d388c3b4417352cef4806
tokin89 <tokin8989@gmail.com>
2022-09-12 10:03:47 +0300

    feat: main-page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2, RendererFactory2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+		@Inject(DOCUMENT) private document: Document,
+		private renderer: Renderer2
+	) { }
 
   ngOnInit(): void {
   }
+
+	public clickChangeTheme(event: MouseEvent): void {
+		
+		// эту логику вынести в сервис
+		
+		if (this.document.body.classList.contains('light-theme')) {
+			this.document.body.classList.remove('light-theme');
+			this.document.body.classList.add('dark-theme');
+		} else if (this.document.body.classList.contains('dark-theme')) {
+			this.document.body.classList.remove('dark-theme');
+			this.document.body.classList.add('light-theme');
+		}
+
+		// эту логику вынести в сервис
+		
+	}
 
 }
